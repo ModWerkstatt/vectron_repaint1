2c81612689c4366c00bc150554897b1200ad6c36
Florian Thiel <flth@flth.de>
2023-12-19 21:37:17 +0100

    Repainter angepasst

--- mod.lua
+++ mod.lua
@@ -13,20 +13,20 @@ return {
 		    },
 		    {
 		        name = "Blanki",
-		        role = "CREATOR",
+		        role = "REPAINTER",
 				--- SETG Lackierungen ---
 		    },
 		    {
 		        name = "LoneWolf",
-		        role = "CREATOR",
+		        role = "REPAINTER",
 		    },
 		    {
 		        name = "BahnBraunschweig-TV",
-		        role = "CREATOR",
+		        role = "REPAINTER",
 		    },
 		    {
 		        name = " MrOnic",
-		        role = "CREATOR",
+		        role = "REPAINTER",
 		    },
 		},
 		tags = { "europe", "train", "siemens", "vectron", "smartron", "electric", "multi-system", "universal" },
@@ -52,49 +52,49 @@ return {
 				key = "br193_ms",
 				name = _("Ms_br193"),
 				values = { "No", "Yes", },
-				tooltip = _("option_ms_br193_desc"),				
+				tooltip = _("option_ms_br193_desc"),
 				defaultIndex = 1,
 			},
 			{
 				key = "br193_ac",
 				name = _("Ac_br193"),
 				values = { "No", "Yes", },
-				tooltip = _("option_ac_br193_desc"),				
+				tooltip = _("option_ac_br193_desc"),
 				defaultIndex = 1,
 			},
 			{
 				key = "br193_dc",
 				name = _("dc_br193"),
 				values = { "No", "Yes", },
-				tooltip = _("option_dc_br193_desc"),				
+				tooltip = _("option_dc_br193_desc"),
 				defaultIndex = 1,
 			},
 			{
 				key = "br193_sm",
 				name = _("Sm_br193"),
 				values = { "No", "Yes", },
-				tooltip = _("option_sm_br193_desc"),				
+				tooltip = _("option_sm_br193_desc"),
 				defaultIndex = 1,
 			},
 			{
 				key = "br193_dachinl",
 				name = _("dachinl_br193"),
 				values = { "No", "Yes", },
-				tooltip = _("option_dachinal_br193_desc"),				
+				tooltip = _("option_dachinal_br193_desc"),
 				defaultIndex = 1,
 			},
 			{
 				key = "br193_osteuropa",
 				name = _("osteuropa_br193"),
 				values = { "No", "Yes", },
-				tooltip = _("option_osteuropa_br193_desc"),				
+				tooltip = _("option_osteuropa_br193_desc"),
 				defaultIndex = 1,
 			},
 			{
 				key = "br193_skandinavien",
 				name = _("skandinavien_br193"),
 				values = { "No", "Yes", },
-				tooltip = _("option_skandinavien_br193_desc"),				
+				tooltip = _("option_skandinavien_br193_desc"),
 				defaultIndex = 1,
 			},
         },
@@ -102,114 +102,113 @@ return {
 	options = {
 	},
 	runFn = function (settings, modParams)
-		
-		local vorspannFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.vorspann == true then				
+
+		local vorspannFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.vorspann == true then
 				data.metadata.availability.yearFrom = 1
 				data.metadata.availability.yearTo = 2
 				--return false
-			end	
+			end
 			--return true
 			return data
-		end 
-		
-		local fakeFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.fake == true then		
+		end
+
+		local fakeFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.fake == true then
 				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
-			return data
-		end 
-		
-		local msFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.ms == true then			
-				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
-			return data
-		end 
-		
-		local acFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.ac == true then			
-				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
+				data.metadata.availability.yearTo = 2
+			end
 			return data
 		end
-		
-		local dcFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.dc == true then			
+
+		local msFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.ms == true then
 				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
-			return data
-		end  
-		
-		local smFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.sm == true then			
-				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
+				data.metadata.availability.yearTo = 2
+			end
 			return data
 		end
-		
-		local dachFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.dach == true then			
+
+		local acFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.ac == true then
 				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
+				data.metadata.availability.yearTo = 2
+			end
 			return data
-		end 
-		
-		local ostFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.ost == true then			
+		end
+
+		local dcFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.dc == true then
 				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
+				data.metadata.availability.yearTo = 2
+			end
 			return data
-		end 
-		
-		local skandiFilter = function(fileName, data)		
-			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.skandi == true then			
+		end
+
+		local smFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.sm == true then
 				data.metadata.availability.yearFrom = 1
-				data.metadata.availability.yearTo = 2				
-			end				
+				data.metadata.availability.yearTo = 2
+			end
 			return data
-		end 
-		
+		end
+
+		local dachFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.dach == true then
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2
+			end
+			return data
+		end
+
+		local ostFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.ost == true then
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2
+			end
+			return data
+		end
+
+		local skandiFilter = function(fileName, data)
+			if data.metadata.transportVehicle and data.metadata.vectron and data.metadata.vectron.skandi == true then
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2
+			end
+			return data
+		end
+
 		if modParams[getCurrentModId()] ~= nil then
-			local params = modParams[getCurrentModId()]					
-			if params["br193_vorspann"] == 0 then				
-				--addFileFilter("model/transportVehicle", vorspannFilter)	
+			local params = modParams[getCurrentModId()]
+			if params["br193_vorspann"] == 0 then
 				addModifier("loadModel", vorspannFilter)
 			end
-			if params["br193_fake"] == 0 then				
+			if params["br193_fake"] == 0 then
 				addModifier("loadModel", fakeFilter)
 			end
-			if params["br193_ms"] == 0 then				
+			if params["br193_ms"] == 0 then
 				addModifier("loadModel", msFilter)
 			end
-			if params["br193_ac"] == 0 then				
+			if params["br193_ac"] == 0 then
 				addModifier("loadModel", acFilter)
 			end
-			if params["br193_dc"] == 0 then				
+			if params["br193_dc"] == 0 then
 				addModifier("loadModel", acFilter)
 			end
-			if params["br193_sm"] == 0 then				
+			if params["br193_sm"] == 0 then
 				addModifier("loadModel", smFilter)
 			end
-			if params["br193_dachinl"] == 0 then				
+			if params["br193_dachinl"] == 0 then
 				addModifier("loadModel", dachFilter)
 			end
-			if params["br193_osteuropa"] == 0 then				
+			if params["br193_osteuropa"] == 0 then
 				addModifier("loadModel", ostFilter)
 			end
-			if params["br193_skandinavien"] == 0 then				
+			if params["br193_skandinavien"] == 0 then
 				addModifier("loadModel", skandiFilter)
 			end
-			
+
 		else
-			--addFileFilter("model/transportVehicle", vorspannFilter)			
+			--addFileFilter("model/transportVehicle", vorspannFilter)
 			addModifier("loadModel", fakeFilter)
 			addModifier("loadModel", msFilter)
 			addModifier("loadModel", acFilter)
@@ -218,7 +217,7 @@ return {
 			addModifier("loadModel", dachFilter)
 			addModifier("loadModel", ostFilter)
 			addModifier("loadModel", skandiFilter)
-		end			
+		end
 	end
 }
 end
