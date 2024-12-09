64dd88aef99dbca5ceb83f5766e2bad1bd97f52a
Dylan7654 <dylan.kreuzinger@gmail.com>
2024-12-09 20:45:50 +0100

    Unnötige Einträge in String gelöscht

--- strings.lua
+++ strings.lua
@@ -3,37 +3,7 @@ return {
 	en = {
 
 		["mod_name"] = "Siemens Vectron MS und AC Repaint Pack 1",
-		["mod_desc"] = "This is the repaint package for the Siemens Vectron MS and AC from the ModWerkstatt. There are 89 paint finishes included in this package, SBB paint finishes were created in cooperation and under permission of SBB Cargo International and Railcolor.",
-
-        ["br193_ms_menu"] = "Vectron class 193 MS",
-        ["br193_ac_menu"] = "Vectron class 193 AC",
-        ["br193_fake_ms_menu"] = "Vectron class 193 MS FAKES",
-        ["br193_fake_ac_menu"] = "Vectron class 193 AC FAKES",
-		["br193_desc"] = "The Vectron is a modern three-phase locomotive for international passenger and freight transport in Europe. In 2014 the Vectron MS was presented for the first time with a speed of 200 km/h (also available with 160 km/h) and a continuous output of 6400 kW.",
-
-		["br192_menu"] = "Smartron class 192",
-		["br192_fake_menu"] = "Smartron class 192",
-		["br192_desc"] = "The Smartron is a slimmed down version of the Vectron. The Smartron is only available in blue metallic, the power is 5600 kW and is only approved for 140 km/h, the locomotive only runs in one country at a time. Smartron has the train protection systems PZB/LZB and ETCS for Germany.",
-
-		["br193_ms"] = 	"Vectron as a multi-system locomotive with 4 pantographs",
-		["br193_ac"] = 	"Vectron as a multi-system locomotive with 2 pantographs",
-		["br193_zza"] = "Vectron as an AC locomotive with 2 pantographs and a train destination indicator",
-		["br192"] = 	"Smartron as AC locomotive with 2 pantographs",
-
-		["Ms_br193"] = "Activate Vectron class 193 multi-system?",
-		["option_ms_br193_desc"] = "Should the Vectron class 193 be activated for the multi-system (pantos down, no light, no driver)?",
-		["Ac_br193"] = "Activate Vectron class 193 AC system?",
-		["option_ac_br193_desc"] = "Should the Vectron class 193 be activated for the AC system (pantos down, no light, no driver)?",
-		["dc_br193"] = "Activate Vectron class 193 DC system?",
-		["option_dc_br193_desc"] = "Should the Vectron class 193 be activated for the DC system (Pantos down, no light, no driver)?",
-		["Sm_br193"] = "Activate Smartron class 192?",
-		["option_sm_br193_desc"] = "Should the Smartron class 192 Pantos bottom, no light, no driver) be activated?",
-		["dachinl_br193"] = "Activate Vectron class 193 with DACHINL equipment?",
-		["option_dachinal_br193_desc"] = "Should the Vectron class 193 be activated with DACHINL equipment (pantos down, no lights, no driver)?",
-		["osteuropa_br193"] = "Activate Vectron class 193 with Eastern Europe equipment?",
-		["option_osteuropa_br193_desc"] = "Should the Vectron class 193 be activated with Eastern European equipment (pantos down, no lights, no driver)?",
-		["skandinavien_br193"] = "Activate Smartron Vectron class 193 with Scandinavia equipment?",
-		["option_skandinavien_br193_desc"] = "Should the Vectron class 193 be activated with Scandinavian equipment (pantos down, no lights, no driver)?",
+		["mod_desc"] = "This is the repaint package for the Siemens Vectron MS and AC from the ModWerkstatt. There are 70 paint finishes included in this package.",
 
 		["br193_ms_siemens_name"] = 	"Class 193 901 MS Siemens",
 		["br193_ms_siemens2_name"] = 	"Class 6193 400 MS Siemens (Vectron 230 km/h)",
@@ -181,9 +151,7 @@ return {
 
 
         -- Vorspann
-        ["Vorspann_br193"] = "Active lead engines?",
-        ["option_vorspann_br193_desc"] = "Active the lead engines (with front panto raised)?",
-
+		
 		["br193_ac_siemens_name_vorspann"] = 	"Lead - Class 192 961 AC Siemens",
 		["br193_ac_siemens2_name_vorspann"] = 	"Lead - Class 193 780 AC Siemens Vectron 230 km/h",
 
@@ -248,8 +216,6 @@ return {
 		["br193_dc_mrce_name_vorspann"] = 		"Lead - Class 191 022 MRCE (Italy)",
 
         -- Fake
-        ["Fake_br193"] = "Active fake engines?",
-        ["option_fake_br193_desc"] = "Active the fake engines (pantos down, no light, no driver)?",
 
 		["br193_ms_siemens_name_fake"] = 	"Fake - Class 193 901 MS Siemens",
 		["br193_ms_siemens2_name_fake"] = 	"Fake - Class 6193 400 MS Siemens (Vectron 230 km/h)",
@@ -399,39 +365,7 @@ return {
 	de = {
 
 		["mod_name"] = "Siemens Vectron MS und AC Repaint Paket 1",
-		["mod_desc"] = "Dies ist das Repaintpaket für die Siemens Vectron MS und AC von der ModWerkstatt. In diesem Paket sind weitere 89 Lackierungen enthalten. Lackierungen der SBB entstanden in Kooperation und mit Erlaubnis der SBB Cargo International und Railcolor.",
-
-        ["br193_ms_menu"] = "Vectron BR 193 MS",
-		["br193_ac_menu"] = "Vectron BR 193 AC",
-		["br193_dc_menu"] = "Vectron BR 193 DC",
-        ["br193_fake_ms_menu"] = "Vectron BR 193 MS Fakes",
-        ["br193_fake_ac_menu"] = "Vectron BR 193 AC Fakes",
-        ["br193_fake_dc_menu"] = "Vectron BR 193 DC Fakes",
-		["br193_desc"] = "Der Vectron ist eine moderne Drehstromlokomotive für den internationalen Personen- und Güterverkehr in Europa. 2014 wurde erstmal der Vectron MS vorgestellt mit einer Geschwindigkeit von 200 km/h (auch mit 160 km/h erhältlich) und einer Dauerleistung von 6400 kW.",
-
-		["br192_menu"] = "Smartron BR 192",
-		["br192_fake_menu"] = "Smartron BR 192",
-		["br192_desc"] = "Die Smartron ist eine abgespeckte Version der Vectron. Die Smartron ist nur in blau metallic erhältlich, die Leistung beträgt 5600 kW und ist nur auf 140 km/h zugelassen, die Lok darf auch immer nur in einem Land verkehren. Die Smartron besitzt für Deutschland die Zugsicherungssysteme PZB/LZB und ETCS. ",
-
-		["br193_ms"] = 	"Vectron als Mehrsystem-Lok mit 4 Stromabnehmern",
-		["br193_ac"] = 	"Vectron als Wechselstrom-Lok mit 2 Stromabnehmern",
-		["br193_zza"] = "Vectron als Wechselstrom-Lok mit 2 Stromabnehmern und Zugzielanzeiger",
-		["br192"] = 	"Smartron als Wechselstrom-Lok mit 2 Stromabnehmern",
-
-		["Ms_br193"] = "Vectron BR 193 Mehrsystem aktivieren?",
-		["option_ms_br193_desc"] = "Soll die Vectron BR 193 für das Mehrsystem (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
-		["Ac_br193"] = "Vectron BR 193 Wechselstromsystem aktivieren?",
-		["option_ac_br193_desc"] = "Soll die Vectron BR 193 für das Wechselstromsystem (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
-		["dc_br193"] = "Vectron BR 193 Gleichstromsystem aktivieren?",
-		["option_dc_br193_desc"] = "Soll die Vectron Br 193 für das Gleichstromsystem (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
-		["Sm_br193"] = "Smartron BR 192 aktivieren?",
-		["option_sm_br193_desc"] = "Soll die Smartron BR 192 (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
-		["dachinl_br193"] = "Vectron BR 193 mit DACHINL Ausrüstung aktivieren?",
-		["option_dachinal_br193_desc"] = "Soll die Vectron BR 193 mit DACHINL Ausrüstung (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
-		["osteuropa_br193"] = "Vectron BR 193 mit Osteuropa Ausrüstung aktivieren?",
-		["option_osteuropa_br193_desc"] = "Soll die Vectron BR 193 mit Osteuropa Ausrüstung (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
-		["skandinavien_br193"] = "Smartron Vectron BR 193 mit Skandinavien Ausrüstung aktivieren?",
-		["option_skandinavien_br193_desc"] = "Soll die Vectron BR 193 mit Skandinavien Ausrüstung  (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
+		["mod_desc"] = "Dies ist das Repaintpaket für die Siemens Vectron MS und AC von der ModWerkstatt. In diesem Paket sind weitere 70 Lackierungen enthalten.",
 
 		["br193_ms_siemens_name"] = 	"BR 193 901 MS Siemens",
 		["br193_ms_siemens2_name"] = 	"BR 6193 400 MS Siemens (Vectron 230 km/h)",
@@ -577,8 +511,6 @@ return {
 
 
         -- Vorspann
-        ["Vorspann_br193"] = "Vorspann-Loks aktivieren?",
-        ["option_vorspann_br193_desc"] = "Die Vorspann-Loks (mit vorderem Panto gehoben) aktiviert werden?",
 
 		["br193_ac_siemens_name_vorspann"] = 	"Vorspann - BR 192 961 AC Siemens",
 		["br193_ac_siemens2_name_vorspann"] = 	"Vorspann - BR 193 780 AC Siemens Vectron 230 km/h",
@@ -646,8 +578,6 @@ return {
 		["br193_dc_mrce_name_vorspann"] = 		"Vorspann - BR 191 022 MRCE (Italien)",
 
         -- Fake
-        ["Fake_br193"] = "Fake-Loks aktivieren?",
-        ["option_fake_br193_desc"] = "Die Fake-Loks (Pantos unten, kein Licht, kein Fahrer) aktiviert werden?",
 
 		["br193_ms_siemens_name_fake"] = 	"Fake - BR 193 901 MS Siemens",
 		["br193_ms_siemens2_name_fake"] = 	"Fake - BR 6193 400 MS Siemens (Vectron 230 km/h)",
